81f979034a29fe7574580ae8e75cd2efaf294f9c
LandonB5 <landon.b5@icloud.com>
2023-10-10 14:17:47 -0400

    try

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3b7feaa08": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
