b2ccdc7422ca2c08ecf831988617cf2eb073420d
LandonB5 <landon.b5@icloud.com>
2023-11-30 20:25:45 -0500

    ok

--- src/ui-components/NoteCardCollection.jsx
+++ src/ui-components/NoteCardCollection.jsx
@@ -10,7 +10,7 @@ import { listNotes } from "../graphql/queries";
 import NoteCard from "./NoteCard";
 import { getOverrideProps } from "./utils";
 import { Collection, Pagination, Placeholder } from "@aws-amplify/ui-react";
-import { API,Storage } from "aws-amplify";
+import { API } from "aws-amplify";
 const nextToken = {};
 const apiCache = {};
 export default function NoteCardCollection(props) {
@@ -61,16 +61,6 @@ export default function NoteCardCollection(props) {
       newCache.push(...result.items);
       newNext = result.nextToken;
     }
-    const notesFromAPI = result.items
-       await Promise.all(
-              notesFromAPI.map(async (note) => {
-                if (note.image) {
-                  const url = await Storage.get(note.name);
-                  note.image = url;
-                }
-                return note;
-              })
-            );
     const cacheSlice = isPaginated
       ? newCache.slice((page - 1) * pageSize, page * pageSize)
       : newCache;
